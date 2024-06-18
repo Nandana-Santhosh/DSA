0e6561f6391bca04444ec7a0e63150ae34941066
Nandana Santhosh <nandanapsmail@gmail.com>
2024-06-18 12:23:15 +0530

    Update valid BST.py

--- LEETCODE/june/TREE/valid BST.py
+++ LEETCODE/june/TREE/valid BST.py
@@ -38,3 +38,23 @@ class Solution:
             
             return (valid(node.left,left,node.val) and valid(node.right,node.val,right)) 
         return valid(root,float("-inf"),float("inf"))
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right 
+class Solution:
+    def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        def dfs(node,left,right):
+            if not node:
+                return True
+            elif  not (node.val>left and node.val< right):
+                return False
+            else:
+                return  dfs(node.left,left,node.val) and dfs(node.right,node.val,right)
+            
+         
+        return dfs(root,float("-inf"),float("inf"))
